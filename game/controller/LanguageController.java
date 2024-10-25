53ff77a25ac0eb697f022c5e404fa8c3b411f5cf
Birk Nordbo <36029360+nordbobirk@users.noreply.github.com>
2024-10-25 15:20:48 +0200

    feat: ui controller with lang

---
+++ game/controller/LanguageController.java
@@ -0,0 +1,24 @@
+package game.controller;
+
+public class LanguageController {
+    
+    private static LanguageController instance;
+
+    private Language language;
+
+    private LanguageController() {
+        this.language = Language.ENGLISH;
+    }
+
+    public static LanguageController getInstance() {
+        if (instance == null) {
+            instance = new LanguageController();
+        }
+        return instance;
+    }
+
+    public Language getLanguage() {
+        return this.language;
+    }
+
+}
---
+++ game/controller/UIController.java
@@ -0,0 +1,28 @@
+package game.controller;
+
+public class UIController {
+
+    private static UIController instance;
+
+    private LanguageController languageController;
+
+    private UIController() {
+        this.languageController = LanguageController.getInstance();
+    }
+
+    public static UIController getInstance() {
+        if (instance == null) {
+            instance = new UIController();
+        }
+        return instance;
+    }
+
+    public static void print(String message) {
+        System.out.print(message);
+    }
+
+    public static void println(String message) {
+        System.out.println(message);
+    }
+
+}
