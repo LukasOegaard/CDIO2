c09016e2273f03f079aebd1e1d28c0dcf67e2bcc
JustBobber <bobberen@protonmail.com>
2024-10-25 13:58:08 +0200

    feat: dieController

--- game/controller/DieController.java
+++ game/controller/DieController.java
@@ -1,2 +1,22 @@
-package game.controller
+package game.controller;
 
+import model.Die;
+
+puplic class DieController(){
+
+    Die die1; 
+    Die die2;
+
+    public void rollBoth(){
+        die1.roll();
+        die2.roll();
+    }
+
+    public int getSum(){
+        int sum = die1.getFaceValue();
+        sum += die2.getFaceValue();
+
+        return sum;
+    }
+
+}
