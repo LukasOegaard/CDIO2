d2b971cfaab80c2f8b3e972115f22b0e6f30673a
JustBobber <bobberen@protonmail.com>
2024-10-28 11:07:49 +0100

    feat: setup of Players

--- game/Game.java
+++ game/Game.java
@@ -1,16 +1,31 @@
 package game;
 
+import game.model.Player;
+import game.controller.DieController;
+import game.controller.PlayerController;
+import game.controller.TileController;
+import game.controller.UIController;
+
+
 public class Game {
     
     private static Game instance;
     private boolean running;
-
+    private PlayerController playerController;
+    private DieController dieController;
+    private TileController tileController;
+    private UIController uiController;
+    
     public static void main(String[] args) {
         getInstance().startGame();
     }
 
     private Game() {
         this.running = true;
+        this.playerController = PlayerController.getInstance();
+        this.dieController = DieController.getInstance();
+        this.tileController = TileController.getInstance();
+        this.uiController = UIController.getInstance();
     }
 
     public static Game getInstance() {
@@ -21,8 +36,25 @@ public class Game {
     }
 
     public void startGame() {
+        setup();
+
         while (this.running) {
             
         }
+    }
+
+    private void setup() {
+
+        /* setup players + accounts via playerController() 
+        *  setup Die via DieController()
+        *  setup 
+        *  setup Language via languageController() 
+        */
+        
+        for (Player player: this.playerController.getPlayers()) {
+            player.setName(this.uiController.promptForPlayerName(player));
+
+        }
+        
     }
 }
