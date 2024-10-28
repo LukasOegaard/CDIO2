9fcfedaa51fc9516834cb95b7b2d1d0786589771
JustBobber <bobberen@protonmail.com>
2024-10-28 10:02:40 +0100

    feat: view package + Utils

---
+++ game/view/Utils.java
@@ -0,0 +1,13 @@
+package view;
+
+public class Utils {
+
+    public static void println(String message) {
+        System.out.println(message);
+    }
+
+    public static void print(String message) {
+        System.out.print(message);
+    }
+
+}
